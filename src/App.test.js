4aadbbd1e0dfe0b2b4b65a7fca0982dc58debc72
Sailesh Kasaju <leosailesh@gmail.com>
2020-11-10 11:59:10 +0545

    add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,7 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('it should fail', () => {
+  expect(2).toEqual(1);
+});
